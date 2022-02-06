8bcd3746cee1e5e7c747182d183ddf2d62300190
Alissa Luk <alissaluk1995@gmail.com>
2022-02-06 13:27:41 -0800

    implemented form and input

--- src/pages/create_account.tsx
+++ src/pages/create_account.tsx
@@ -1,16 +1,36 @@
 import Head from 'next/head';
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
 import styles from 'src/styles/create_account.module.scss';
 
 export default function CreateAccount() {
+  const [username, setUsername] = useState('');
+  const [userError, setUserError] = useState(null);
+  const [password, setPassword] = useState('');
+  const [passError, setPassError] = useState(null);
+
   async function handleSubmit(evt: FormEvent) {
     evt.preventDefault();
     const response = await fetch('/api/create_new_account', {
       method: 'POST',
-      body: JSON.stringify({}),
+      body: JSON.stringify({username, password}),
     });
+    let data = await response.json();
+    console.log(data)
+    if (data.result === false) {
+      setUserError(data.errors.username || null);
+      setPassError(data.errors.password || null);
+    } else {
+      setUserError(null);
+      setPassError(null);
+    }
+  }
 
-    console.log(await response.json());
+  function changeHandler (evt) { //TYPE SCRIPT FOR INPUT?
+    if (evt.target.name === 'username') {
+      setUsername(evt.target.value);
+    } else {
+      setPassword(evt.target.value);
+    }
   }
 
   return (
@@ -20,6 +40,33 @@ export default function CreateAccount() {
       </Head>
       <article className={styles.article}>
         <form className={styles.form} onSubmit={handleSubmit}>
+          <h1>Create New Account</h1>
+          <div>
+            <label
+              className={styles.label}
+              htmlFor="username">Username:</label>
+            <input
+              className={styles.input}
+              onChange={changeHandler}
+              type="text"
+              id="username"
+              name="username">
+            </input>
+            {userError ? <div className={styles.error}>{userError}</div> : null}
+          </div>
+          <div>
+            <label
+              className={styles.label}
+              htmlFor="password">Password:</label>
+            <input
+              className={styles.input}
+              onChange={changeHandler}
+              type="password"
+              id="password"
+              name="password">
+            </input>
+            {passError ? <div className={styles.error}>{passError}</div> : null}
+          </div>
           <button>Create Account</button>
         </form>
       </article>
